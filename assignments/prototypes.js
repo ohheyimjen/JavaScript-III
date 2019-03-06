7c345d4425ff0ee1dccde1c45ae77a99461025ff
ohheyimjen <jendority@gmail.com>
2019-03-06 15:28:21 -0800

    Completed all but Principle 4 code example for Explicit Binding

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(object) {
+  this.createdAt = object.createdAt;
+  this.name = object.name;
+  this.dimensions = object.dimensions;
+  this.destroy = function() {
+    return `${this.name} was removed from the game.`;
+  };
+}
 
 /*
   === CharacterStats ===
@@ -22,7 +30,12 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-
+function CharacterStats(attributes) {
+  this.healthPoints = attributes.healthPoints;
+  this.takeDamage = function () {
+    return '<object name> took damage.';
+  };
+}
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -32,6 +45,15 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(character) {
+  this.team = character.team;
+  this.weapons = character.weapons;
+  this.language = character.language;
+  this.greet = function() {
+    return `<object name> offers a greeting in <object language>.`;
+  };
+
+}
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +63,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+// /*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +124,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,25 +1,52 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: This is the default for the "this" keyword. It bonds to the global object.
+
+* 2. Implicit Binding: Points "this" to a window object. It binds to objects when it is used in methods.
+
+* 3. New Binding: Used when an object is created and called back in a constructor function. ONLY used with constructor functions.
+
+* 4. Explicit Binding: Can force a function call to use a particular object for "this" binding. We explicitly say to a function what object it should use for "this", using call, apply and bind.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function makeFood(food) {
+    console.log(this);
+    return food;
+}
+makeFood('pizza');
 
 // Principle 2
 
-// code example for Implicit Binding
+const myPizza = {
+    customize: 'Add some',
+    getBaked: function(topping) {
+        console.log(`${this.customize} ${topping} to my pizza pie!`);
+        console.log(this);
+    }
+ };
+ myPizza.getBaked('extra cheese');
 
 // Principle 3
 
-// code example for New Binding
+function foodOrder(food) {
+    this.foodWanted = 'May I please have ';
+    this.food = food;
+    this.order = function() {
+      console.log(this.foodWanted + this.food);
+      console.log(this);
+    };
+  }
+  
+  const burger = new foodOrder('Two cheeseburgers with fries');
+  const pizza = new foodOrder('One extra large pizza with everything');
+  
+  burger.order();
+  pizza.order();
 
 // Principle 4
 
